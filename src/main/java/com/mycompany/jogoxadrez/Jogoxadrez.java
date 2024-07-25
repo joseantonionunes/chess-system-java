57ef57cc64fc8f846fff1211a7634dd041a1f2a0
joseantonionunes <joseantonionunes2018@gmail.com>
2024-07-25 15:14:21 -0300

    Printing possible moves

--- src/main/java/com/mycompany/jogoxadrez/Jogoxadrez.java
+++ src/main/java/com/mycompany/jogoxadrez/Jogoxadrez.java
@@ -18,21 +18,25 @@ public class Jogoxadrez {
         ChessMatch chessMatch = new ChessMatch();
 
         while (true) {
-            try{
+            try {
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces());
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
 
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
-                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);  
-            }catch (ChessException e) {
-                System.out.println(e.getMessage());
-                sc.nextLine();
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }
+            catch (ChessException e) {
+                    System.out.println(e.getMessage());
+                    sc.nextLine();
             }
             catch (InputMismatchException e) {
                     System.out.println(e.getMessage());
